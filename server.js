ef702ac27ff53fba4b1548c099e33fb71997dcd1
CoolShivas <shivas2710cool00@gmail.com>
2025-05-11 15:55:55 +0530

    Formation of Home, About and Contact Page.

--- server.js
+++ server.js
@@ -2,6 +2,32 @@ import express from "express";
 
 const server = express();
 
+server.get("/", (request, response) => {
+    response.send("Welcome to the Home Page.");
+});
+
+server.get("/about", (request, response) => {
+    response.send("Welcome to the About Page.");
+});
+
+server.get("/contact", (request, response) => {
+    return response.send(`<div class="container">
+        <h1>Contact Us</h1>
+        <form id="shorten-form">
+            <div>
+                <label for="url">Name :-</label>
+                <input type="url" name="url" id="url" required>
+            </div>
+            <div>
+                <label for="shortCode">Problem :-</label>
+                <input type="text" name="shortCode" id="shortCode" required>
+            </div>
+            <button type="submit">Resolve</button>
+        </form>
+    </div>`);
+});
+
+
 const PORT = 3001;
 
 server.listen(PORT, () => {
@@ -15,5 +41,5 @@ server.listen(PORT, () => {
 // // // And, you will get the Output as Server Running at 3001
 // // // Finally, your server started :- Server running at 3001;
 // // // Now, open the browser and in url write localhost:3001
-// // // You will get the result on the display this :- Cannot GET /
-// // // You will get the result on the display console this :- GET http://localhost:3001/ 404 (Not Found)
+// // // You will get the result on the display as :- Welcome to the Home Page.
+// // // As well as you can switch the pages as localhost:3001/about or localhost:3001/contact;
