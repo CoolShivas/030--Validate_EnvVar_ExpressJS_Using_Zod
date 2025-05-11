d8c2ddbb470293c729a7f6c2bf367ccac205a5b1
CoolShivas <shivas2710cool00@gmail.com>
2025-05-11 14:15:53 +0530

    Change of theory file name. Use of npm init for the creation of Package.JSON file.

---
+++ theoryZod.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * * * You might have many environment variables in your project, and it's important to validate each one to ensure they are all present and correct.
+ * 
+ * 
+ * * * You can either handle this manually or, preferably, use a validation library like Zod.
+ * 
+ * 
+ * * * Zod is a TypeScript-first schema declaration and validation library that allows you to define and validate the structure of your data easily. 
+ * 
+ * 
+ * * * We will be using Zod for various validation in the future, so we will also use it here to validate environment variables.
+ * 
+ * 
+ * 
+ */
